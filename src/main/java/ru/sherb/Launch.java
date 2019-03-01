5574e96f1abc62c3e62ffa311ced3eb9ac5b508d
mshherbakov <sherbakov.maks@gmail.com>
2019-03-01 22:05:57 +0500

    add https port

--- src/main/java/ru/sherb/Launch.java
+++ src/main/java/ru/sherb/Launch.java
@@ -3,26 +3,20 @@ package ru.sherb;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.meta.logging.BotLogger;
 import ru.sherb.bot.TranslateBot;
-
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 
 public class Launch {
 
     private static final String MAIN_TAG = "Main";
 
     public static void main(String[] args) throws TelegramApiRequestException {
-        BotLogger.setLevel(Level.ALL);
-        BotLogger.registerLogger(new ConsoleHandler());
 
         ApiContextInitializer.init();
 
-        BotLogger.debug(MAIN_TAG, "get port: " + System.getenv("PORT"));
-        TelegramBotsApi api = new TelegramBotsApi("https://perch-tg-bots.herokuapp.com", "localhost:" + System.getenv("PORT"));
+        System.out.println("Port: " + System.getenv("PORT"));
+        TelegramBotsApi api = new TelegramBotsApi("https://perch-tg-bots.herokuapp.com:443", "localhost:" + System.getenv("PORT"));
 
-        BotLogger.debug(MAIN_TAG, "get bot name: " + System.getenv("TG_BOT_NAME"));
+        System.out.println("Bot name: " + System.getenv("TG_BOT_NAME"));
         api.registerBot(new TranslateBot(System.getenv("TG_BOT_NAME"), System.getenv("TG_BOT_TOKEN"), System.getenv("TG_BOT_NAME")));
     }
 }
