d1eb1d0ef495291a9eeafe62fce2560138a1dd06
mshherbakov <sherbakov.maks@gmail.com>
2019-03-01 23:33:43 +0500

    debug

--- src/main/java/ru/sherb/Launch.java
+++ src/main/java/ru/sherb/Launch.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import ru.sherb.bot.TranslateBot;
 
+import java.text.MessageFormat;
+
 public class Launch {
 
     private static final String MAIN_TAG = "Main";
@@ -17,6 +19,8 @@ public class Launch {
         TelegramBotsApi api = new TelegramBotsApi("https://perch-tg-bots.herokuapp.com/", "http://localhost:" + System.getenv("PORT") + "/");
 
         System.out.println("Bot name: " + System.getenv("TG_BOT_NAME"));
+        System.out.println("Bot token: " + System.getenv("TG_BOT_TOKEN"));
+        System.out.println("Bot url: " + MessageFormat.format("{0}callback/", "https://perch-tg-bots.herokuapp.com/") + System.getenv("TG_BOT_NAME"));
         api.registerBot(new TranslateBot(System.getenv("TG_BOT_NAME"), System.getenv("TG_BOT_TOKEN"), System.getenv("TG_BOT_NAME")));
     }
 }
