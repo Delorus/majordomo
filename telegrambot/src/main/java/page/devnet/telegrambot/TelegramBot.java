aac9fdbbb3062dc9b779b36c854040bc986ed5fe
mshherbakov <sherbakov.maks@gmail.com>
2019-11-21 00:38:24 +0500

    super fix 8

--- telegrambot/src/main/java/page/devnet/telegrambot/TelegramBot.java
+++ telegrambot/src/main/java/page/devnet/telegrambot/TelegramBot.java
@@ -60,6 +60,7 @@ class TelegramBot {
         @Override
         public BotApiMethod onWebhookUpdateReceived(Update update) {
             if (update.hasMessage() && isBeforeStart(update.getMessage())) {
+                log.warn("skip message: [{}], that got before starting: [start: {}, got: {}]", update.getMessage().getText(), startTime, update.getMessage().getDate());
                 return null;
             }
 
