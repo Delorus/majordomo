f8920acfed6293b0e5d5e0d8d2708c9d7715f54e
Maksim Shherbakov <mshherbakov@naumen.ru>
2019-11-20 17:19:51 +0500

    super fix 3

--- telegrambot/src/main/java/page/devnet/telegrambot/TelegramBot.java
+++ telegrambot/src/main/java/page/devnet/telegrambot/TelegramBot.java
@@ -12,6 +12,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import page.devnet.pluginmanager.MessageSubscriber;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import java.util.List;
 
